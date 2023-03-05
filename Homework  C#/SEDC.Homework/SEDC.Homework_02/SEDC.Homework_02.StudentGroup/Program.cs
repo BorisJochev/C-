4b15bbb378cbeb79c1a28cf18187d8201c9e3e5c
BorisJochev <119973977+BorisJochev@users.noreply.github.com>
2023-03-06 00:26:10 +0100

    Add files via upload

---
+++ Homework  C#/SEDC.Homework/SEDC.Homework_02/SEDC.Homework_02.StudentGroup/Program.cs
@@ -0,0 +1,33 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+string[] studentsG1 = { "Petko", "Petranka", "Stole", "Mile", "Stojna" };
+
+string[] studentsG2 = { "Monika", "Zlatko", "Sara", "Toso", "Mitra" };
+
+
+
+
+Console.WriteLine("Enter student group: (1 / 2)");
+int num = Int32.Parse(Console.ReadLine());
+
+if (num == 1)
+{		Console.WriteLine("The Students in G1 are: ");
+	foreach (string name in studentsG1)
+	{
+		
+		Console.WriteLine(name);
+	}
+}
+else if (num == 2)
+{		Console.WriteLine("The Students in G2 are: ");
+	foreach (string name in studentsG2)
+	{
+		
+		Console.WriteLine(name);
+	}
+}
+else
+{
+	Console.WriteLine("Not valid!");
+}
---
+++ Homework  C#/SEDC.Homework/SEDC.Homework_02/SEDC.Homework_02.SumOfEven/Program.cs
@@ -0,0 +1,46 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+
+int[] arrayNum = new int[6];
+
+
+Console.WriteLine("Enter integer no.1: ");
+int num1 = Int32.Parse(Console.ReadLine());
+arrayNum[0] = num1;
+
+Console.WriteLine("Enter integer no.2: ");
+int num2 = Int32.Parse(Console.ReadLine());
+arrayNum[1] = num2;
+
+Console.WriteLine("Enter integer no.3: ");
+int num3 = Int32.Parse(Console.ReadLine());
+arrayNum[2] = num3;
+
+Console.WriteLine("Enter integer no.4: ");
+int num4 = Int32.Parse(Console.ReadLine());
+arrayNum[3] = num4;
+
+Console.WriteLine("Enter integer no.5: ");
+int num5 = Int32.Parse(Console.ReadLine());
+arrayNum[4] = num5;
+
+Console.WriteLine("Enter integer no.6: ");
+int num6 = Int32.Parse(Console.ReadLine());
+arrayNum[5] = num6;
+
+
+
+int sum = 0;
+
+foreach (int num in arrayNum)
+{
+
+	if (num % 2 == 0)
+	{
+
+		sum += num;
+
+	}
+}
+Console.WriteLine("The result is:  " + sum);
